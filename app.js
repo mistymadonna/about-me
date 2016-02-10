1208e313ee02e8fa47918375f4c06593bfdd0a33
Misty Nguyen <mistymadonna@gmail.com>
2016-02-10 09:10:54 -0800

    finished html, tweaked intro to JS and added two JS questions.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-// JS file for Feb 9 class demo
 // A guessing game about you with 5 questions for the user, and a different response to the user depending upon if the y got the right or wrong answer.
 
 var userName = prompt('What is your name?');
-console.log('The user said their name is ' + userName);
+console.log('Their name is ' + userName);
 
-alert('Hi there, ' + userName + ', I\'m going to ask you some questions about me. Please answer Y or N');
+alert('Hey y\'all, and hey ' + userName + ', Let\'s get to know each other. I\'m going to ask you some questions about me. Please answer Y or N.');
 
-var answer1 = prompt('Was Sam born in South Dakota?');
-console.log('The user answer to Question 1 was ' + answer1);
+var answer1 = prompt('Am I from Seattle?');
+console.log('Answer to Question 1 was ' + answer1);
 if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
-  alert('Nice! I was borned in Germany');
+  alert('Nice! Memphis born and raised.');
   console.log(userName + ' nailed Question 1');
-} else if (answer1 === 'Y') {
-  alert('Sorry, you\'re wrong!!!');
+} else if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() ==='YES') {
+  alert('Nope!!! ' +userName);
 } else {
-  alert('You should have given a Y or N answer, so you lose this round.');
+  alert('C\'mon now.. I said answer with Y or N');
 }
