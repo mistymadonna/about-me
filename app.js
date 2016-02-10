4764ca466b54e2e38e296bd2a8ad149411242ff7
Misty Nguyen <mistymadonna@gmail.com>
2016-02-10 11:00:26 -0800

    added a few things inc. a !

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var userName = prompt('What is your name?');
 console.log('Their name is ' + userName);
 
-alert('Hey y\'all, and hey ' + userName + ', Let\'s get to know each other. I\'m gonna ask you some questions about me. Please answer Y or N. Let\'s do this');
+alert('Hey y\'all, and hey ' + userName + ', Let\'s get to know each other. I\'m going to ask you some questions about me. Please answer Y or N. Here we go!');
 
 var answer1 = prompt('Am I from Seattle?');
 console.log('Answer to Question 1 was ' + answer1);
@@ -11,7 +11,7 @@ if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
   alert('Nice! Memphis born and raised.');
   console.log(userName + ' nailed Question 1');
 } else if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() ==='YES') {
-  alert('Nope, '  +userName);
+  alert('Nope, '  + userName + ' !');
 } else {
   alert('C\'mon now.. I said answer with Y or N');
 }
@@ -38,7 +38,7 @@ if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'YES') {
 } else {
   alert ('You are no fun today.');
 }
-// Make sure to do good console.log statements.
+// Make sure to do good console.log statements
 var answer4 = prompt('Do I have a pet?');
 console.log('Question 4 ' + answer4);
 if (answer4.toLowerCase() === 'n' || answer4.toUpperCase() === 'NO') {
