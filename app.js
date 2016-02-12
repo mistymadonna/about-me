0318683ed9ef2a7a4716577b8d2be9b18af61c29
Nolan Arendt <nolanarendt10@gmail.com>
2016-02-12 09:30:10 -0800

    Added homepage link and questions on seperate html

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ console.log('Their name is ' + userName);
 
 alert('Hey y\'all, and hey ' + userName + ', Let\'s get to know each other. I\'m going to ask you some questions about me. Please answer Y or N. Here we go!');
 
+var pTagOne = document.getElementById("ans-name");
+
 function seattleHome() {
   var answer1 = prompt('Am I from Seattle?');
   console.log('Answer to Question 1 was ' + answer1);
   if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
-    alert('Nice! Memphis born and raised.');
+    //alert('Nice! Memphis born and raised.');
     console.log(userName + ' nailed Question 1');
+    pTagOne.textContent = 'Nice! Memphis born and raised';
   } else if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() === 'YES') {
-    alert('Nope, ' + userName + ' !');
+    //alert('Nope, ' + userName + ' !');
+    pTagOne.textContent = 'Nope, ' + userName + ' !';
   } else {
     alert('C\'mon now.. I said answer with Y or N');
   }
