88497f7a3c2a1d65f6ec19f61e2bf8b6ec95d4d6
Nolan Arendt <nolanarendt10@gmail.com>
2016-02-12 10:09:56 -0800

    Finishedand all functions work

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('Their name is ' + userName);
 
 alert('Hey y\'all, and hey ' + userName + ', Let\'s get to know each other. I\'m going to ask you some questions about me. Please answer Y or N. Here we go!');
 
-var pTagOne = document.getElementById("ans-one");
+var pTagOne = document.getElementById('ans-one');
 
 function seattleHome() {
   var answer1 = prompt('Am I from Seattle?');
@@ -18,55 +18,80 @@ function seattleHome() {
     //alert('Nope, ' + userName + ' !');
     pTagOne.textContent = 'Nope, ' + userName + ' !';
   } else {
-    alert('C\'mon now.. I said answer with Y or N');
+    //alert('C\'mon now.. I said answer with Y or N');
   }
 }
 seattleHome();
 
-// function beachWeather() {
-//   var answer2 = prompt('Do I like hot weather?');
-//   console.log('Question 2 answer ' + answer2);
-//   if (answer2.toLowerCase() === 'n' || answer2.toUpperCase() === 'NO') {
-//     alert('Not even close.');
-//     console.log(userName + ' incorrect answer');
-//   } else if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'YES') {
-//     alert('You\'re awesome and correct.');
-//     console.log(userName + ' correct answer');
-//   } else {
-//     alert('You gotta follow the instructions now.');
-//   }
-// }
-// function outdoorsFan() {
-//   var answer3 = prompt('Am I outdoorsy?');
-//   console.log('Question 3 answer ' + answer3);
-//   if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'YES') {
-//     alert('Yep!');
-//     console.log(userName + ' correct answer again');
-//   } else if (answer3.toLowerCase() === 'n' || answer3.toUpperCase() === 'NO') {
-//     alert('Sorry, nope.');
-//   } else {
-//     alert('You are no fun today.');
-//   }
-// }
-// function hasPet() {
-//   var answer4 = prompt('Do I have a pet?');
-//   console.log('Question 4 ' + answer4);
-//   if (answer4.toLowerCase() === 'n' || answer4.toUpperCase() === 'NO') {
-//     alert('Wrongo.');
-//   } else if (answer4.toLowerCase() === 'y' || answer4.toUpperCase() === 'YES') {
-//     alert('You\'re good at this!');
-//   } else {
-//     alert('So, you\'re a comedian today.');
-//   }
-// }
-// function seafoodOpinion() {
-//   var answer5 = prompt('Do I like seafood?');
-//   console.log('the important question ' + answer5);
-//   if (answer5.toLowerCase() === 'y' || answer5.toUpperCase() === 'YES') {
-//     alert('Dude! Pike Place Chowder is the bomb!');
-//   } else if (answer5.toLowerCase() === 'n' || answer5.toUpperCase() === 'NO') {
-//     alert('You don\'t know me at all, sucka.');
-//   } else {
-//     alert('Lame-o.');
-//   }
-// }
+var pTagTwo = document.getElementById('ans-two');
+
+function beachWeather(answer) {
+  if (answer.toLowerCase() === 'n' || answer.toUpperCase() === 'NO') {
+    //alert('Not even close.');
+    console.log(userName + ' incorrect answer');
+    pTagTwo.textContent = 'Not even close';
+  } else if (answer.toLowerCase() === 'y' || answer.toUpperCase() === 'YES') {
+    //alert('You\'re awesome and correct.');
+    console.log(userName + ' correct answer');
+    pTagTwo.textContent = 'You\'re awesome and correct.';
+  } else {
+    //alert('You gotta follow the instructions now.');
+  }
+}
+var answer2 = prompt('Do I like hot weather?');
+beachWeather(answer2);
+
+
+var pTagThree = document.getElementById('ans-three');
+
+function outdoorsFan(answer) {
+  //var answer3 = prompt('Am I outdoorsy?');
+  if (answer.toLowerCase() === 'y' || answer.toUpperCase() === 'YES') {
+    //console.log('Question 3 answer ' + answer3);
+    //alert('Yep!');
+    console.log(userName + ' correct answer again');
+    pTagThree.textContent = 'Yep!';
+  } else if (answer.toLowerCase() === 'n' || answer.toUpperCase() === 'NO') {
+    //alert('Sorry, nope.');
+    pTagThree.textContent = 'Sorry, nope.';
+  } else {
+    //alert('You are no fun today.');
+  }
+}
+var answer3 = prompt('Am I outdoorsy?');
+outdoorsFan(answer3);
+
+var pTagFour = document.getElementById('ans-four');
+
+function hasPet(answer) {
+  console.log('Question 4 ' + answer4);
+  if (answer.toLowerCase() === 'n' || answer.toUpperCase() === 'NO') {
+    //alert('Wrongo.');
+    pTagFour.textContent = 'Wrongo.';
+  } else if (answer.toLowerCase() === 'y' || answer.toUpperCase() === 'YES') {
+    //alert('You\'re good at this!');
+    pTagFour.textContent = 'You\'re good at this!';
+  } else {
+    //alert('So, you\'re a comedian today.');
+  }
+}
+var answer4 = prompt('Do I have a pet?');
+hasPet(answer4);
+
+var pTagFive = document.getElementById('ans-five');
+
+function seafoodOpinion(answer) {
+  console.log('the important question ' + answer5);
+  if (answer.toLowerCase() === 'y' || answer.toUpperCase() === 'YES') {
+    //alert('Dude! Pike Place Chowder is the bomb!');
+    pTagFive.textContent = 'Dude! Pike Place Chowder is the bomb!';
+  } else if (answer.toLowerCase() === 'n' || answer.toUpperCase() === 'NO') {
+    //alert('You don\'t know me at all, sucka.');
+  } else {
+    pTagFive.textContent = 'You don\'t know me at all, sucka.';
+    //alert('Lame-o.');
+  }
+}
+
+var answer5 = prompt('Do I like seafood?');
+seafoodOpinion(answer5);
